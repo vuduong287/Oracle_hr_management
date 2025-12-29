688bd78f9b75518455b82bb5facaaa0ab64a90c6
vuduong287 <trangbom287@gmail.com>
2025-12-29 17:48:25 +0100

    xong

--- app.js
+++ app.js
@@ -9,9 +9,18 @@ const updatedeptmanagerRoutes = require('./routes/departments');
 const app = express();
 app.use(bodyParser.json());
 const cors = require('cors');
+const path = require('path');
 
 app.use(cors());
+/* ===============================
+   SERVE FRONTEND (index.html)
+=============================== */
+app.use(express.static(path.join(__dirname, 'frontend')));
 
+/* Trang chủ */
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
+});
 // API cần đăng nhập Oracle
 app.use('/api/employees', oracleAuth, insertRoutes);
 app.use('/api/employees', oracleAuth,employeeRoutes);
@@ -19,5 +28,5 @@ app.use('/api/employees', oracleAuth, deleteRoutes);
 app.use('/api/employees', oracleAuth, updateRoutes);
 app.use('/api/departments', oracleAuth, updatedeptmanagerRoutes);
 app.listen(3000, () => {
-  console.log('Server running on port 3000');
+  console.log('Server running on http://localhost:3000');
 });
